25d8bca6629079be9e4f2795c69eb20a85f37e1f
Reena-porsiya <138755077+Reena-porsiya@users.noreply.github.com>
2024-05-17 17:51:01 +0530

    c

---
+++ advanced_java/src/advanced_java/OptionalExample.java
@@ -0,0 +1,12 @@
+package advanced_java;
+
+import java.util.Optional;
+
+public class OptionalExample {
+    public static void main(String[] args) {
+        Optional<String> optionalName = Optional.ofNullable(null);
+
+        // Using Optional to handle the absence of value
+        System.out.println(optionalName.orElse("No name provided"));
+    }
+}
