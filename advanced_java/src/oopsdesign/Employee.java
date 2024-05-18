b69bf32f46b39e3ce3dfe702f392f3043e2f664e
Reena-porsiya <138755077+Reena-porsiya@users.noreply.github.com>
2024-05-18 16:34:35 +0530

    save

---
+++ advanced_java/src/oopsdesign/Employee.java
@@ -0,0 +1,35 @@
+package oopsdesign;
+
+
+public class Employee {
+    private String name;
+    private int id;
+
+    // Constructor
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", id=" + id + "]";
+    }
+}
