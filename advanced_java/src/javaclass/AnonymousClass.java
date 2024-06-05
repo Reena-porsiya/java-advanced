871a85f20d3d412a1953168d5fc93d27530d9c8d
Reena-porsiya <138755077+Reena-porsiya@users.noreply.github.com>
2024-06-05 14:34:57 +0530

    change

--- advanced_java/src/javaclass/AnonymousClass.java
+++ advanced_java/src/javaclass/AnonymousClass.java
@@ -1,16 +1,18 @@
-package javaclass;
-interface Greeting {
-    void greet();
-}
-
-public class AnonymousClass {
-    public static void main(String[] args) {
-        Greeting greeting = new Greeting() {
-            @Override
-            public void greet() {
-                System.out.println("Hello from Anonymous Class");
-            }
-        };
-        greeting.greet();
-    }
-}
+//package javaclass;
+//
+//interface Greeting {
+//    
+//void greet();
+//}
+//
+//public class AnonymousClass {
+//    public static void main(String[] args) {
+//        Greeting greeting = new Greeting() {
+//           // @Override
+//            public void greet() {
+//                System.out.println("Hello from Anonymous Class");
+//            }
+//        };
+//        greeting.greet();
+//    }
+//}
